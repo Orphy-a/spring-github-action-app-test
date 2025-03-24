f0529c8333365e57f58392b8d92751fce8996cf7
jaeyeong <jaeyoung0560@gmail.com>
2025-03-24 18:13:01 +0900

    commit 0.0.1

---
+++ src/test/java/kr/co/orphy/springgithubactionapptest/SpringGithubActionAppTestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.orphy.springgithubactionapptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionAppTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
